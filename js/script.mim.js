3a562140e0d9961a7c25effdfe98f88c916bc520
JANDIRA PIRES <piresjandira1@gmail.com>
2019-09-03 16:36:59 -0300

    Criação da função mensagem

---
+++ js/script.mim.js
@@ -0,0 +1,8 @@
+function mensagem(){
+    nome = prompt("Digite o se nome");
+    email = prompt("Digite o se email");
+    idade = prompt("Digite o se idade");
+
+    var saida = "Nome:"+nome+"<br>Email:"+email+"<br>Idade:"+idade;
+    return saida;
+}
